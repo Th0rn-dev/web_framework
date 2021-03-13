c8b83b037e2f7c6ebbb01a64deae83c90dfe10c4
Th0rn-dev <solnek33@gmail.com>
2021-03-13 22:58:01 +0300

    add routes for app

---
+++ app.py
@@ -0,0 +1,14 @@
+from api import API
+
+app = API()
+# app()
+
+
+@app.route("/home")
+def home(request, response):
+    response.text = "Hello from HOME page"
+
+
+@app.route("/about")
+def about(request, response):
+    response.text = "Hello from ABOUT page"
